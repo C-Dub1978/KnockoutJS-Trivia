548bbff0986f1bd4b7ced2d0160df2ba8c263a96
TransientCW <chris@myubercode.com>
2019-03-04 21:53:04 -0700

    still working on loading custom components dynamically

--- view-models/view-model.js
+++ view-models/view-model.js
@@ -1,4 +1,4 @@
-function CurrentQuestionModel(question, answers, correctAnswer, id) {
+function QuestionModel(question, answers, correctAnswer, id) {
   var self = this;
   self.question = question;
   self.answers = answers;
@@ -9,6 +9,8 @@ function CurrentQuestionModel(question, answers, correctAnswer, id) {
 function OverviewModel() {
   var self = this;
 
+  self.currentId = 1;
+
   self.userName = ko.observable("");
 
   self.userNameSet = ko.observable(false);
@@ -17,18 +19,48 @@ function OverviewModel() {
 
   self.answeredQuestions = ko.observableArray([]);
 
+  self.questionsObjects = [];
+
   self.currentQuestion = null;
 
   self.submitted = ko.observable(false);
 
-  self.loadQuestionIntoView = function(id) {
-    const index = self.answeredQuestions().findIndex(obj => {
-      return obj.id === parseInt(id);
+  self.loadQuestions = function() {
+    console.log("loading questions");
+    questionsAndAnswers.forEach(obj => {
+      self.questionsObjects.push(
+        new QuestionModel(obj.question, obj.answers, obj.correctAnswer, obj.id)
+      );
     });
-    if (index !== -1) {
-      self.currentQuestion = self.answeredQuestions()[index];
-    }
   };
+
+  self.loadQuestionComponent = function() {
+    const container = document.getElementById("questions-container");
+    const questionObject = self.questionsObjects[self.currentId - 1];
+    // container.innerHTML = `
+    //   <questions-answers
+    //     params="triviaQuestion: questionObject.question,
+    //     triviaAnswersArray: questionObject.answers,
+    //     correctAnswer: questionObject.correctAnswer,
+    //     id: questionObject.id"></questions-answers>
+    // `;
+    container.innerHTML = `
+      <questions-answers
+        params="triviaQuestion: 'question',
+        triviaAnswersArray: ['answer1', 'answer2', 'answer3'],
+        correctAnswer: 'correct answer',
+        id: '1'"></questions-answers>
+    `;
+  };
+
+  // self.loadQuestionIntoView = function(id) {
+  //   const index = self.answeredQuestions().findIndex(obj => {
+  //     return obj.id === parseInt(id);
+  //   });
+  //   if (index !== -1) {
+  //     self.currentQuestion = self.answeredQuestions()[index];
+  //   }
+  // };
 
   self.setUserName = ko.computed({
     read: function() {
@@ -36,6 +68,8 @@ function OverviewModel() {
     },
     write: function() {
       self.userNameSet(true);
+      self.loadQuestions();
+      self.loadQuestionComponent();
     },
     owner: self
   });
@@ -55,9 +89,24 @@ function OverviewModel() {
   });
 
   ko.components.register("questions-answers", {
-    viewModel: CurrentQuestionModel,
+    // viewModel: QuestionModel,
+    viewModel: function(params) {
+      console.log("params: ", params);
+      var self = this;
+      self.triviaQuestion = ko.observable(params.triviaQuestion);
+      // self.triviaAnswers = ko.observableArray(params.triviaAnswersArray);
+      self.triviaAnswers = params.triviaAnswersArray;
+
+      self.correctAnswer = params.correctAnswer;
+
+      self.id = params.id;
+    },
     template: `
-      <p></p>
+      <h4>Question:</h4><pre data-bind="text: question">
+      <h4>Answers:</h4>
+      <div data-bind="foreach: answers">
+        <input type="radio" data-bind="attr: { name: id }" /><pre data-bind="text: $data">
+      </div>
     `
   });
 }
