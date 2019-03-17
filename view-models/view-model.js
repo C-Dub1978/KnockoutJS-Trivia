d4a9c39a94c0d2bb580401e6fba3866c409853cb
TransientCW <chris@myubercode.com>
2019-03-17 08:40:07 -0600

    all scaffolding in place for dynamically loading landing view, questions view, scrolling through questions, submitting questions, and restarting after results view loaded

--- view-models/view-model.js
+++ view-models/view-model.js
@@ -5,13 +5,21 @@ function MainViewModel() {
   // Reference to 'this'
   const self = this;
   // Length 10 array of questionsAnswers objects
-  self.questionsArray = populateQuestions();
+  self.questionsArray = ko.observableArray(populateQuestions());
   // Username
   self.userName = ko.observable("");
-  // Username set boolean flag
-  self.userNameSet = ko.observable(false);
-  // All answers submitted
+  // All answers submitted flag for rendering the results template
   self.answersSubmitted = ko.observable(false);
+  // Current id in the questionsArray
+  self.currentId = ko.observable(0);
+  // Current template view
+  self.currentTemplate = ko.observable("landing-container");
+  // Progress through the trivia questions
+  self.progress = ko.observable("");
+  // Percentage of questions correct
+  self.resultsPercent = ko.observable(0);
+
+  // HELPER FUNCTIONS
   // Username setter
   self.submitUsername = function() {
     let inputName = $("#userName").val();
@@ -19,10 +27,41 @@ function MainViewModel() {
       inputName = "Player 1";
     }
     self.userName(inputName);
-    self.userNameSet(true);
+    self.currentTemplate("qa-container");
   };
-  self.currentQuestionNumber = ko.observable(3);
-  self.progress = ko.observable("15%");
+
+  // Click handler for previous button
+  self.navigatePrevious = function() {
+    const id = parseInt(self.currentId());
+    if (id > 0) {
+      self.currentId(id - 1);
+    }
+  };
+
+  // Click handler for next button
+  self.navigateNext = function() {
+    const id = parseInt(self.currentId());
+    if (id < self.questionsArray().length - 1) {
+      self.currentId(id + 1);
+    }
+  };
+
+  // Click handler for grading submission
+  self.submitAll = function() {
+    self.currentTemplate("results-container");
+    self.answersSubmitted(true);
+  };
+
+  // Click handler for restart button
+  self.restartGame = function() {
+    self.questionsArray(populateQuestions());
+    self.userName("");
+    self.answersSubmitted(false);
+    self.currentId(0);
+    self.currentTemplate("landing-container");
+    self.progress("");
+    self.resultsPercent(0);
+  };
 }
 
 /**
